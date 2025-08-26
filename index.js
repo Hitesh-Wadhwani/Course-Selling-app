88ae6b8799998d25639a8302c9bd94975d92ba68
Hitesh-Wadhwani <hiteshwadwani555@gmail.com>
2025-08-26 13:56:21 +0530

    Added admin routes

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 
 const { userRouter } = require('./routes/user.js')
 const { courseRouter } = require('./routes/course.js')
-
+const { adminRouter } = require('./routes/admin.js')
 
 app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
-app.use('/api/v1/course', courseRouter)
+app.use('/api/v1/admin', adminRouter)
+app.use('/api/v1/course', courseRouter);
+
 
 
 app.listen(3000, () => {
---
+++ routes/admin.js
@@ -0,0 +1,40 @@
+const {Router} = require('express');
+const adminRouter = Router()
+
+
+
+adminRouter.post('/signup', function(req, res){
+    res.json({
+        message : "Signup Endpoint"
+    })
+})
+
+adminRouter.post('/signin', function(req,res) {
+    res.json({
+        message : "Signup endpoint"
+    })
+})
+
+adminRouter.post('/course', function(req, res){
+    res.json({
+        message : "Signup Endpoint"
+    })
+})
+
+adminRouter.put('/course', function(req, res) {
+    res.json({
+        message : "Signup endpoint"
+    })
+})
+
+
+adminRouter.get('/course/bulk', function(req, res) {
+    res.json({
+        message : "Signup endpoint"
+    })
+})
+
+
+module.exports = {
+    adminRouter : adminRouter
+}
